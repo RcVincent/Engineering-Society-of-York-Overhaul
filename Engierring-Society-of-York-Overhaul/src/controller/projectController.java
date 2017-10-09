007d71da8b7dfc8230d2c6ece1a992de097c2cae
Ryan <Ryan@Ryan-PC>
2017-10-08 20:37:42 -0400

    filled out the project controller and the login servlet

--- Engierring-Society-of-York-Overhaul/src/controller/projectController.java
+++ Engierring-Society-of-York-Overhaul/src/controller/projectController.java
@@ -1,5 +1,34 @@
 package controller;
 
-public class projectController {
+import java.util.ArrayList;
+import java.util.List;
 
+import database.IDatabase;
+import database.DatabaseProvider;
+import database.DerbyDatabase;
+
+import model.User;
+
+public class projectController {
+	private IDatabase db;
+	
+	public projectController() {
+		DatabaseProvider.setInstance(new DerbyDatabase());
+		db = DatabaseProvider.getInstance();
+	}
+
+	
+	public static boolean authenticate(User u, String pswd)
+	{
+		boolean real = false;
+		if(u.getPassword().equals(pswd)){
+
+			real = true;
+		}
+
+		return real;
+	}
+	
+	
+	//have the option for later options 
 }
